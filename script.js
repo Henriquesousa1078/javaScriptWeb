b5df0dc7da1ca5737e3f87a48eb3e4442c1a5081
henriquesousa1078 <henriquesousa107890@gmail.com>
2024-02-29 19:59:47 -0300

    primeiro commit do js

---
+++ script.js
@@ -0,0 +1,53 @@
+const calabreso = document.getElementById("main-input")
+
+console.log(calabreso)
+
+const paragraph = document.getElementsByClassName("paragraph-js")
+
+console.log(paragraph)
+
+const givetag = document.getElementsByTagName("h1")
+
+console.log(givetag)
+
+const tagName = document.getElementsByName("nome-completo")
+
+console.log(tagName)
+
+const element = document.querySelector("#main-input")
+
+element.placeholder = "Vai se fuder"
+element.value = "num quero nao"
+
+console.log(element.src)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const element = document.querySelector(".paragraph-js")
+
+
+console.log(element.textContent)
+console.log(element.innerText)
+console.log(element.innerHTML)
